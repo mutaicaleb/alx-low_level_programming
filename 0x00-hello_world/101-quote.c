ffe94f9c8055f40f5d722097ccc4c7ccf81f336f
mutaicaleb <calebyobr@gmail.com>
2023-02-19 09:29:57 -0800

    printing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
- * The main function of the program.
- * Return:Always 1
+/*
+ * This program demonstrates how to print text to the standard error using the write function.
+ *
+ * Usage: ./program
+ *
+ * Returns: 1 on success, 0 on failure.
  */
 int main(void)
 {
