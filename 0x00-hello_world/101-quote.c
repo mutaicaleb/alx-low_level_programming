a758db9d66efd4e72483ecedf2b95b4c854de694
mutaicaleb <calebyobr@gmail.com>
2023-02-19 09:45:35 -0800

    printing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - Prints a line of code to a new line to an error
  *
  * Return:Always 1
- * */
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
