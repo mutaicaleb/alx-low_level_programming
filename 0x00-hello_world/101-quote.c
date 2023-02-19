66e98c0c3387877490fee21cbeaf42dd60a504f5
mutaicaleb <calebyobr@gmail.com>
2023-02-19 09:44:28 -0800

    printing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
- /**
- * main - Entry point for the program
+/**
+ * main - Prints a line of code to a new line to an error
  *
- * Description: This function is the entry point for the program. It prints the
- *              message "and that piece of art is useful" - Dora Korpar,
- *              2015-10-19 to the standard error using the write function.
- *
- * Return: Always 1
- */
+ * Return:Always 1
+ * */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
