7a7eedd9e516144008990712fec6bb76bc518ae9
mutaicaleb <calebyobr@gmail.com>
2023-02-19 09:10:52 -0800

    printing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * The main function of the program.
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful" - Dora Korpar, "2015-10-19",59);
+	return (1);
+}
