dc18ca054076bc2ce6f3057da5c0963ff444e796
mutaicaleb <calebyobr@gmail.com>
2023-02-20 04:09:27 -0800

    lower and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,14 @@ int main()
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++) 
+	for (c = 'a'; c <= 'z'; c++)
 {
 	putchar(c);
 }
-
 	for (c = 'A'; c <= 'Z'; c++) 
 {
 	putchar(c);
 }
-
 	putchar('\n');
-	return 0;
+	return(0);
 }
