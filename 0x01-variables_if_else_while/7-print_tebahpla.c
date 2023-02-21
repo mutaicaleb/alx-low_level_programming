8588d5226171e963bd0c34469ecc02f9da09130f
mutaicaleb <calebyobr@gmail.com>
2023-02-20 16:23:14 -0800

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return:0 indicates successful execution of the program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar ('\n');
+	return (0);
+}
