d26d4a583dffab6198dc669de2a9193cdff1774a
mutaicaleb <calebyobr@gmail.com>
2023-02-20 17:06:05 -0800

    numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,12 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i; j < 10; j++)
-		{
-			putchar(i + 48);
-			putchar(j + 48);
-			if (i != 9 || j !=9)
-			{
-				putchar(',');
-			}
-		}
+		putchar(i + 48);
+		putchar(',');
 	}
 	putchar('\n');
 	return (0);
