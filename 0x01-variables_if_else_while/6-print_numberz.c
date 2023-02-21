cf6a6808800b27e3954546dadb1a7d1d005b2e48
mutaicaleb <calebyobr@gmail.com>
2023-02-20 16:14:12 -0800

    int 1-10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return:0 indicates successful execution of the program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+		return (0);
+}
