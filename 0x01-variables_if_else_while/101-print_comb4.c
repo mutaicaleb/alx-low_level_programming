94eedb9fb98ab4ab1187372ac330510a7184479a
mutaicaleb <calebyobr@gmail.com>
2023-02-20 17:44:57 -0800

    comb_4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - entry point
- *
  * Return: always 0 (success)
  */
 int main(void)
 {
-	int i = 48;
-	int j = 48;
-	int k = 48;
 	int f, g, h;
 
-	while (i < 58)
 	for (f = '0'; f <= '9'; f++)
 	{
-		j = i + 1;
-		while (j < 58)
 		for (g = 0; g <= '9'; g++)
 		{
-			k = j + 1;
-			while (k < 58)
 			for (h = 0; h <= '9'; h++)
 			{
-				putchar(i);
-				putchar(i);
-				putchar(i);
-				if (i < 55 || j < 56 || k < 57)
 				if (f < g && g < h)
 				{
-					putchar(44);
-					putchar(32);
 					putchar(f);
 					putchar(g);
 					putchar(h);
@@ -41,13 +25,9 @@ int main(void)
 						putchar(' ');
 					}
 				}
-				k++;
 			}
-			j++;
 		}
-		i++;
 	}
-	putchar(10);
 	putchar('\n');
 	return (0);
 }
