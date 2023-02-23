c76b197fe79bd9d83aadbaf3efa920bf49b8f121
mutaicaleb <calebyobr@gmail.com>
2023-02-23 12:34:17 -0800

    _isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - function declaration
+ * Description: test if digit
+ * @c: value to check
+ * Return: 1 for true, 0 for fals
+ */
+int _isdigit(int c)
+{
+
+	/**
+ * _isdigit - function declaration
+ * Description: test if digit
+ * @c: value to check
+ * Return: 1 for true, 0 for fals
+ */
+	for (c = 0; c <= 9; c++)
+	{
+		if ((c == 'a') && (c != '1')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
