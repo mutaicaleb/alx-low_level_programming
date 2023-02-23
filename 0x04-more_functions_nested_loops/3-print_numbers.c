d3c67d57ea23aaef1fe197a15fd6bedaa535bdb1
mutaicaleb <calebyobr@gmail.com>
2023-02-23 13:57:32 -0800

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - function declaration
+ * Description: print 0 - 9
+ * Return: void
+ */
+
+void print_numbers(void);
+
+void print_numbers(void)
+{
+	int i;
+	 for (i=0; i<=9; i++)
+	 {
+		 _putchar(i + '0');
+	 }
+	  _putchar('\n');
+}
+
