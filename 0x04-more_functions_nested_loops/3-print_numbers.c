aa3033bec56fb86a1101647f2a65bbe7984b93c6
mutaicaleb <calebyobr@gmail.com>
2023-02-23 14:02:31 -0800

    print numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,10 +10,11 @@ void print_numbers(void);
 void print_numbers(void)
 {
 	int i;
-	 for (i=0; i<=9; i++)
-	 {
+
+	for (i = 0; i <= 9; i++)
+	{
 		 _putchar(i + '0');
-	 }
+	}
 	  _putchar('\n');
 }
 
