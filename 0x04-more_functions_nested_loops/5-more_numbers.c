c56a9ad3e7992498d64291e6fa26d8d07a178112
mutaicaleb <calebyobr@gmail.com>
2023-02-25 07:52:58 -0800

    numbers 0 - 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * more_numbers - function declaration
+ * Description: print 1-14, 10 times
+ * Return: void
+ */
+
+void more_numbers(void);
+
+/**
+ * more_numbers - function definition
+ * Description: prints 1-14, ten times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int m;
+	int n;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar( / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
