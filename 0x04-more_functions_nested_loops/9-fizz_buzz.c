5b963096405eb5511039fabbc537e8abb9218fc2
mutaicaleb <calebyobr@gmail.com>
2023-02-26 06:13:08 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "main.h"
 
 /**
@@ -8,12 +9,34 @@
  * @range: the end number
  * Return: void
  */
-int main(void)
 
+void fizz_buzz(int range);
+
+/**
+ * main -entry point
+ * Description: calls fizz_buzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	fizz_buzz(100);
+	return (0);
+}
+
+/**
+ * fizz_buzz - function definition
+ * Description: print fizz or buzz for multiples of 3
+ * and 5
+ * @range: the ending number
+ * Return: void
+ */
+
+void fizz_buzz(int range)
 {
 	int number;
 
-	for (number = 1; number <= 100; number++)
+	for (number = 1; number <= range; number++)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
 		{
