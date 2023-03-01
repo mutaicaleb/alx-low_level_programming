6847be18e89a455f4cb7f4d414ed4eee14bafab8
mutaicaleb <calebyobr@gmail.com>
2023-02-28 19:26:45 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function declaration
+ * @a: array pointer
+ * @n: number of elements to print
+ * Return: void
+ */
+
+void print_array(int *a, int n);
+
+/**
+ * print_array - function definition
+ * Description: prints specified array elements
+ * @a: the array pointer
+ * @n: the number of elements to print
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+		if (index != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+
