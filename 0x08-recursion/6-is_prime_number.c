0df5926680f4b85bd2460cb063b2162c55760782
mutaicaleb <calebyobr@gmail.com>
2023-03-08 18:59:29 -0800

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,20 +8,20 @@ int is_prime_number(int n)
 {
 if (n == 1 || n < 0)
 return (0);
-return (number(2, n));
+return (pnumber(2, n));
 
 }
 /**
-* number - help me to find the prime number
+* pnumber - help me to find the prime number
 * @x: integer
 * @n: integer
 * Return: 1 if prime or 0 if not
 **/
-int number(int x, int n)
+int pnumber(int x, int n)
 {
 	if (n == x)
 		return (1);
 	else if (n % x == 0)
 		return (0);
-	return (number(x + 1, n));
+	return (pnumber(x + 1, n));
 }
