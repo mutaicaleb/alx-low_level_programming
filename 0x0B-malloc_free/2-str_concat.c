e5c5662edc6de4a6e91aac158f345fec98648197
mutaicaleb <calebyobr@gmail.com>
2023-03-15 13:48:09 -0700

    concatenating strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,29 +2,47 @@
 #include <stdlib.h>
 
 /**
- * str_concat - a function that concatenates strings
- *@s1: string which should be emptied
- *@s2: string should add to another one
- *Return:concatenated string
- *
+ * str_concat - function declaration
+ * @s1: string 1
+ * @s2: string 2
+ * Return: as string value
+ */
+
+char *str_concat(char *s1, char *s2);
+
+/**
+ * str_concat - function definition
+ * @s1: the first string
+ * @s2: the second string
+ * Description: concatenates s1 and s2
+ * Return: the concatenated string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j,  len;
-	char fstring;
+	char *fstring;
+	int i;
+	int j;
+	int len = 0;
 
-	while (s1[len] || s2[len])
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
-	fstring = (char *)malloc(sizeof(char) * len)
 
-	if (fstring == 0)
+	fstring  = malloc(sizeof(char) * len);
+
+	if (fstring == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
 		fstring[j++] = s1[i];
 
-	for (index = 0; s2[i]; i++)
+	for (i = 0; s2[i]; i++)
 		fstring[j++] = s2[i];
 
 	return (fstring);
