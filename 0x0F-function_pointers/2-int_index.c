d3a994a447d782921ff9700459f892958e94a13b
mutaicaleb <calebyobr@gmail.com>
2023-03-27 11:01:18 -0700

    search intergers function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- * int_index - indexes the array
- * @array : array to be checked
- * @size : size of the array
- * @cmp : the function pointer
+ * int_index - indexes array
+ * @array: array to go through
+ * @size: size of array
+ * @cmp: function to do something with size
+ * Return: int value
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= o || cmp == NULL)
-	return (-1);
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-		{
 			return (i);
-		}
-		return (-1);
 	}
+	return (-1);
 }
+
